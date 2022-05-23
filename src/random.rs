9d452794d3ecea1118ba35adf93aeaa077d679b9
Princic-1837592 <andreapri.99@gmail.com>
2022-05-23 18:57:21 +0200

    Started preparations for wasm support and version 0.3

---
+++ src/random.rs
@@ -0,0 +1,40 @@
+// #[cfg(not(target_family = "wasm"))]
+pub(crate) use rand::{thread_rng, Rng};
+
+
+// #[cfg(not(target_family = "wasm"))]
+pub fn gen_range(rng: &mut impl Rng, min: usize, max: usize) -> usize {
+    rng.gen_range(min..max)
+}
+
+
+// #[cfg(target_family = "wasm")]
+// pub(crate) trait Rng {}
+//
+//
+// #[cfg(target_family = "wasm")]
+// pub(crate) struct RngWrapper;
+//
+//
+// #[cfg(target_family = "wasm")]
+// impl Rng for RngWrapper {}
+//
+//
+// #[cfg(target_family = "wasm")]
+// pub(crate) fn thread_rng() -> RngWrapper {
+//     RngWrapper {}
+// }
+//
+//
+// #[cfg(target_family = "wasm")]
+// #[wasm_bindgen]
+// extern "C" {
+//     #[wasm_bindgen(js_namespace = Math)]
+//     fn random() -> f64;
+// }
+//
+//
+// #[cfg(target_family = "wasm")]
+// pub fn random_range(_: &mut impl Rng, min: usize, max: usize) -> usize {
+//     (random() * (max - min) as f64).floor() as usize + min
+// }
